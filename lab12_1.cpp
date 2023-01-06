8ed604b7eb79bbf0a5f3ff1cdf1df462132fbef5
Your Name <you@example.com>
2023-01-06 15:19:55 +0700

    finished lab12-1

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,2 +1,25 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <ctime>
+using namespace std;
+
+int main(){
+    string b,grade;
+    srand(time(0));
+    cout << "Press Enter 3 times to reveal your future.";
+    getline(cin,b);
+    getline(cin,b);
+    getline(cin,b);
+    int n=rand()%9;
+    if(n==0) grade="A";
+    else if(n==1) grade="B+";
+    else if(n==2) grade="B";
+    else if(n==3) grade="C+";
+    else if(n==4) grade="C";
+    else if(n==5) grade="D+";
+    else if(n==6) grade="D";
+    else if(n==7) grade="F";
+    else if(n==8) grade="W";
+    cout << "You will get "<< grade <<" in this 261102.";
+}
